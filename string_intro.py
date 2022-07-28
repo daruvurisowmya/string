e712c764c663c7db87e89c9a823e3c15aa400783
daruvurisowmya <109509763+daruvurisowmya@users.noreply.github.com>
2022-07-28 14:03:18 +0530

    Add files via upload

---
+++ string_intro.py
@@ -0,0 +1,11 @@
+x='department'
+# print(x,type(x))
+# print(x[0])
+# print(x[5])
+# print(x[8])
+# print(x[9])
+# print(x[10]) # IndexError: string index out of range
+# print(x[0]+x[1]+x[2]+x[3]+x[4]+x[5])
+# print(x[3]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7]+x[8]+x[9])
+# print(x[0:6])
+# print(x[3]+x[2:])
